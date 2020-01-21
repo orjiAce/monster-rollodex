420bf037dcaeabdb82b1b6ab7fa6bb0a4f7f8398
orjiAce <orjiace@gmail.com>
2020-01-21 01:57:58 +0100

    Updates

---
+++ precache-manifest.b9d01948ec6c6cf24289081a1f1afc87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835e6cd8e36fcfc02c8c90a2f5f850ce",
+    "url": "/monster-rollodex/index.html"
+  },
+  {
+    "revision": "9968acfba503e4751fad",
+    "url": "/monster-rollodex/static/css/main.ae61fb07.chunk.css"
+  },
+  {
+    "revision": "a678b64d299905494033",
+    "url": "/monster-rollodex/static/js/2.d102072a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rollodex/static/js/2.d102072a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9968acfba503e4751fad",
+    "url": "/monster-rollodex/static/js/main.a2fe047a.chunk.js"
+  },
+  {
+    "revision": "f20fec980142b71589d9",
+    "url": "/monster-rollodex/static/js/runtime-main.566dc70b.js"
+  }
+]);
